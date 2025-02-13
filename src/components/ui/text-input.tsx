c3109b0953ec2576990863dee14a231444f0b8fd
Reid Harrison <reid.al.harrisson@gmail.com>
2025-02-13 22:42:27 +0100

    feat: fix input autofill

--- src/components/ui/text-input.tsx
+++ src/components/ui/text-input.tsx
@@ -2,15 +2,6 @@
 
 import React, { useState } from "react";
 
-const colors = {
-  label: "#6F5883",
-  text: "#5B415B",
-  focused: "#1E90FF",
-  invalid: "#FF0000",
-  border: "#5B415B1A",
-  background: "#FFFFFF",
-};
-
 interface TextInputProps {
   label: string;
   name: string;
@@ -28,14 +19,12 @@ export const TextInput: React.FC<TextInputProps> = ({
   className,
   name,
 }) => {
+  const [value, setValue] = useState<string>("");
   const [state, setState] = useState<States>({
     filled: false,
-    valid: true,
+    valid: value ? true : false,
     focused: false,
   });
-  const [value, setValue] = useState<string>("");
-
-  console.log(state);
 
   const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
     setState({
@@ -56,30 +45,33 @@ export const TextInput: React.FC<TextInputProps> = ({
 
   return (
     <div
-      className={`relative w-full rounded-lg transition-border duration-200 border-solid border-2 border-[${
-        state.valid === false
-          ? colors.invalid
-          : state.filled === true && state.focused === true
-          ? colors.focused
-          : colors.border
-      }] bg-[${colors.background}] ${className}`}
+      className={`
+        relative w-full rounded-lg transition-border duration-200
+        border-solid border-2
+        ${state.valid === false ? "border-[#FF0000]" : ""}
+        ${
+          state.filled && state.focused
+            ? "border-[#1E90FF]"
+            : "border-[#5B415B1A]"
+        }
+        bg-white
+        ${className || ""}
+      `}
     >
       <label
-        className={`absolute font-medium transition-all duration-200 text-[${
+        className={`absolute font-medium transition-all duration-200 ${
           state.valid === false
-            ? colors.invalid
-            : state.filled === true && state.focused === true
-            ? colors.focused
-            : colors.label
-        }] left-5 ${
-          state.filled === true ? "text-xs top-1" : "text-base top-3"
-        }`}
+            ? "text-[#FF0000]"
+            : state.filled && state.focused
+            ? "text-[#1E90FF]"
+            : "text-[#6F5883]"
+        } left-5 ${state.filled ? "text-xs top-1" : "text-base top-3"}`}
       >
         {label}
       </label>
       <input
         name={name}
-        className={`border-none text-base w-full z-10 bg-transparent pt-5 pb-1 px-4 text-[${colors.text}]`}
+        className={`border-none text-base w-full z-10 bg-transparent pt-5 pb-1 px-4 text-[#5B415B]`}
         onFocus={handleFocus}
         onBlur={handleBlur}
         onChange={handleChange}
